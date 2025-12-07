240267727ace47de13662fa0c204933e8dff9ef5
Amir Rosenfeld <amir.rosenfeld@gmail.com>
2025-12-07 09:17:43 +0200

    .

--- src/tima_timer/__init__.py
+++ src/tima_timer/__init__.py
@@ -6,6 +6,6 @@ A desktop productivity timer that cycles through your projects.
 __version__ = "1.0.0"
 __author__ = "Tima Developer"
 
-from tima_timer.app import main
+from tima_timer.app_flet import main
 
 __all__ = ['main']
